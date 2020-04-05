f158282a223dab5eeb8471d562126bbadd78a82e
DoubleWen <tcliuwenwen@126.com>
2020-04-05 12:39:35 +0800

    add py

---
+++ start.py
@@ -0,0 +1,25 @@
+import requests
+# import requests_cache
+import re
+
+
+class JybLinks:
+    url = 'http://www.jyb.cn'
+    session = requests.session()
+
+    def __init__(self, url):
+        # requests_cache.install_cache()
+        # requests_cache.clear()
+        self.url = url
+
+    def get_links(self):
+        url = self.url
+        session = requests.session()
+        response = session.get(url)
+        # print(response.text)
+        links = re.findall(r'href\=\"(http\:\/\/www\.jyb\.cn\/rmtlist[a-zA-Z0-9\.\/]+)\"', response.text)
+        # print(links)
+        links = list(set(links))
+        # for link in links:
+        #     print(link)
+        return links
